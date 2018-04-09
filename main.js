4fab975276ca8d6c0bb8539c9f6b270334b02f0a
Z-dorf <sebastian.zanon2@gmail.com>
2018-04-09 11:23:00 +0200

    it's working, kinda

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
+    var xhr = new XMLHttpRequest();
 
-    // var xhr = new XMLHttpRequest();
+    // On souhaite juste récupérer le contenu du fichier, la méthode GET suffit amplement :
+    xhr.open('GET', 'list.php');
 
-    // // On souhaite juste récupérer le contenu du fichier, la méthode GET suffit amplement :
-    // xhr.open('GET', 'list.php');
+    xhr.addEventListener('readystatechange', function() { // On gère ici une requête asynchrone
 
-    // xhr.addEventListener('readystatechange', function() { // On gère ici une requête asynchrone
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
 
-    //     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
+            document.getElementById('fileContent').innerHTML = '<span>' + xhr.responseText + + '</span>'; // Et on affiche !
+        }
 
-    //         document.getElementById('fileContent').innerHTML = '<span>' + xhr.responseText + + '</span>'; // Et on affiche !
+    });
 
-    //     }
-
-    // });
-
-    // xhr.send(null); // La requête est prête, on envoie tout !
-
-    fetch('list.php').then(function(response){
-        return response.json();
-    }).then (function(tableau){
-        for (entry of tableau){
-            document.getElementById("fileContent").innerHTML +=entry+"<br/>";
-   }
+    xhr.send(null); // La requête est prête, on envoie tout !
+    
+//     fetch('list.php').then(function(response){
+//         return response.json();
+//     }).then (function(tableau){
+//         for (entry of tableau){
+//             document.getElementById("fileContent").innerHTML +=entry+"<br/>";
+//    }
    
-});
+// });
 
   
--- training.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*$iterator = new DirectoryIterator(dirname(__FILE__));
-foreach ($iterator as $fileinfo) {
-    echo $fileinfo->getFilename() . "<br>\n";
-}*/
-echo $_GET["test"];
-echo $_GET["nimp"];
-?>
